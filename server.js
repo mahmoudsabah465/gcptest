8eaf1bfdf64e01dbc2ddcc99061bfc2bc79df5e5
mahmoudsabah465 <32990575+mahmoudsabah465@users.noreply.github.com>
2026-03-18 05:47:50 +0300

    Update and rename app.js to server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const httpProxy = require('http-proxy');
+
+const proxy = httpProxy.createProxyServer({
+    target: 'http://34.42.60.105:80',
+    ws: true,
+    changeOrigin: true
+});
+
+proxy.on('error', (err, req, res) => {
+    if (res && res.writeHead) {
+        res.writeHead(502);
+        res.end('Proxy Error');
+    }
+});
+
+const server = http.createServer((req, res) => {
+    proxy.web(req, res);
+});
+
+server.on('upgrade', (req, socket, head) => {
+    proxy.ws(req, socket, head);
+});
+
+server.listen(process.env.PORT || 8080);
